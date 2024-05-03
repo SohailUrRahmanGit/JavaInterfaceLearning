39c7429495208f10b871121ee36a36231d9975b7
Sohail Ur Rahman S <ssohailmails@gmail.com>
2024-05-03 11:34:12 +0530

    Completed Interface

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Inside Interface Learning");
+        Dog dog1 = new Dog();
+        dog1.makeSound();
+
+        Cat cat1 = new Cat();
+        cat1.makeSound();
     }
 }
